e3843bcdf1a508e858b6d5738962b7e815d8e40e
Isaic Gonzalez <go.isaic@gmail.com>
2023-11-02 22:50:14 -0500

    initialCards array

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "./images/yosemite-valley.png",
+  },
+  {
+    name: "Lake Louise",
+    link: "./images/lake-louise.png",
+  },
+  {
+    name: "Bald Mountains",
+    link: "./images/bald-mountains.png",
+  },
+  {
+    name: "Latemar",
+    link: "./images/latemar.png",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "./images/vanoise-national-park.png",
+  },
+  {
+    name: "Lago di Braies",
+    link: "./images/lago-di-braies.png",
+  },
+];
